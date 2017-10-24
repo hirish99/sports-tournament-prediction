f5fc05f433e818a9ba4cff410deec6bebb14ab7f
hirish99 <32660271+hirish99@users.noreply.github.com>
2017-10-23 20:12:44 -0700

    Add files via upload

--- src/winlossratio.py
+++ src/winlossratio.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tourney = pd.read_csv("./Data_v1_2016/TourneyCompactResults.csv", index_col = 0)
+tourney = pd.read_csv("../data/TourneyCompactResults.csv", index_col = 0)
 
-seasons = pd.read_csv("./Data_v1_2016/Seasons.csv", index_col = 0)
-teams = pd.read_csv("./Data_v1_2016/Teams.csv", index_col = 0)
+teams = pd.read_csv("../data/Teams.csv", index_col = 0)
 
 tourney_2015 = tourney.loc[[2015],:]
 
-wins_2015 = tourney_2015['Wteam'].value_counts().to_frame()
+wins_2015 = tourney_2015['Wteam'].value_counts(dropna = True).to_frame()
 
-losses_2015 = tourney_2015['Lteam'].value_counts().to_frame()
+losses_2015 = tourney_2015['Lteam'].value_counts(dropna = True).to_frame()
 
 team_mappings = pd.DataFrame.to_dict(teams)
 team_mappings = team_mappings['Team_Name']
@@ -21,13 +20,21 @@ losses_2015.rename(team_mappings, inplace=True)
 wins_2015.columns = ['Wins']
 losses_2015.columns = ['Losses']
 
+print(wins_2015)
+print(losses_2015)
+
+wins_2015.fillna(value = 0, inplace = True)
+losses_2015.fillna(value = 0, inplace = True)
 
 wlr = (wins_2015['Wins']/losses_2015['Losses']).to_frame()
 wlr.columns = ['W/L']
 
-#wins_2015.plot(kind = 'bar')
-#losses_2015.plot(kind = 'bar')
+wlr.fillna(value = 0, inplace = True)
+
 wlr.plot(kind = 'bar')
+wlr.loc['Duke']['W/L'] = 6
+print(wlr)
+
 
 plt.show()
 
