2d7f1e8c06ed412c57915f6d943cae5d68f5e634
hirish99 <32660271+hirish99@users.noreply.github.com>
2017-10-23 19:18:51 -0700

    Add files via upload

---
+++ src/input.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tourney = pd.read_csv("./Data_v1_2016/TourneyCompactResults.csv", index_col = 0)
+
+seasons = pd.read_csv("./Data_v1_2016/Seasons.csv", index_col = 0)
+teams = pd.read_csv("./Data_v1_2016/Teams.csv", index_col = 0)
+
+tourney_2015 = tourney.loc[[2015],:]
+
+wins_2015 = tourney_2015['Wteam'].value_counts().to_frame()
+
+losses_2015 = tourney_2015['Lteam'].value_counts().to_frame()
+
+team_mappings = pd.DataFrame.to_dict(teams)
+team_mappings = team_mappings['Team_Name']
+
+wins_2015.rename(team_mappings, inplace=True)
+losses_2015.rename(team_mappings, inplace=True)
+
+wins_2015.columns = ['Wins']
+losses_2015.columns = ['Losses']
+
+
+wlr = (wins_2015['Wins']/losses_2015['Losses']).to_frame()
+wlr.columns = ['W/L']
+
+#wins_2015.plot(kind = 'bar')
+#losses_2015.plot(kind = 'bar')
+wlr.plot(kind = 'bar')
+
+plt.show()
+
+
