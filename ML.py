63e9a9b246f9399abc850006bb8f01197030cf62
hirish99 <32660271+hirish99@users.noreply.github.com>
2017-11-06 19:08:40 -0800

    Add files via upload

---
+++ ML.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from winlossratio import wlr
+import sklearn
+from sklearn.linear_model import LinearRegression
+
+reg_season = pd.read_csv("./data/RegularSeasonDetailedResults.csv", index_col = 0)
+teams = pd.read_csv("./data/Teams.csv", index_col = 0)
+team_mappings = pd.DataFrame.to_dict(teams)
+team_mappings = team_mappings['Team_Name']
+
+#The point of ML.py is to input a year, and give back relevant stats for each
+#team during that year. This creates our label (input) dataframes.
+team_id = 1102
+year = 2005
+stat = 'Wscore'
+
+#Gives a table with 2 columns: Number of losses and Number of Wins
+year_data = reg_season.loc[[year],:]
+
+wins_year = year_data['Wteam'].value_counts(dropna = True).to_frame()
+wins_year.rename(team_mappings, inplace=True)
+wins_year = wins_year.sort_index()
+
+loss_year = year_data['Lteam'].value_counts(dropna = True).to_frame()
+loss_year.rename(team_mappings, inplace=True)
+loss_year = loss_year.sort_index()
+
+table = pd.concat([wins_year, loss_year], axis = 1)
+
+#Adds extra columns to previous table about relevant stats
+#reg_season = pd.read_csv("./data/RegularSeasonDetailedResults.csv", index_col = 2)
+#reg_season.rename(team_mappings, inplace=True)
+#reg_season = reg_season.sort_index()
+
+#Take the subset of our particular team
+#reg_season = reg_season.loc[team_mappings[team_id],:]
+#reg_season = reg_season[reg_season['Season'] == year]
+
+#Average of the team we want:
+#avg = (reg_season.mean()).to_frame()
+#avg = avg.rename(columns = {0:team_mappings[team_id]})
+
+#Particular Stat
+#table[stat] = np.nan
+#table.set_value(team_mappings[team_id], stat ,avg[team_mappings[team_id]][stat])
+
+print(wlr(year))
+table['W'] = wlr(year)
+
+table.fillna(0, inplace = True);
+
+X = table.drop('W', axis = 1)
+
+lm = LinearRegression()
+
+lm.fit(X, table.W)
+
+prediction = lm.predict(X)
+#realval = ((table.W).to_frame()).as_matrix()
+
+
+
+
+
---
+++ winlossratio.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def wlr(year):
+    tourney = pd.read_csv("./data/TourneyCompactResults.csv", index_col = 0)
+
+    teams = pd.read_csv("./data/Teams.csv", index_col = 0)
+
+    tourney_2015 = tourney.loc[[year],:]
+
+    wins_2015 = tourney_2015['Wteam'].value_counts(dropna = True).to_frame()
+
+    losses_2015 = tourney_2015['Lteam'].value_counts(dropna = True).to_frame()
+
+    team_mappings = pd.DataFrame.to_dict(teams)
+    team_mappings = team_mappings['Team_Name']
+
+    wins_2015.rename(team_mappings, inplace=True)
+    losses_2015.rename(team_mappings, inplace=True)
+
+    wins_2015.columns = ['Wins']
+    losses_2015.columns = ['Losses']
+
+    #print(wins_2015)
+    #print(losses_2015)
+
+    wins_2015.fillna(value = 0, inplace = True)
+    losses_2015.fillna(value = 0, inplace = True)
+
+    wlr = (wins_2015['Wins']/losses_2015['Losses']).to_frame()
+    wlr.columns = ['W/L']
+
+    wlr.fillna(value = 0, inplace = True)
+
+    wlr.plot(kind = 'bar')
+    #wlr.loc['Duke']['W/L'] = 6
+    return wlr;
+
+
+
+
+
