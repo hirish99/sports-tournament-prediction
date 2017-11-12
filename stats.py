14244b9153014f75f8bdbeff42de588c94adef6c
hirish99 <32660271+hirish99@users.noreply.github.com>
2017-11-11 17:31:33 -0800

    Add files via upload

--- stats.py
+++ stats.py
@@ -1,39 +1,33 @@
 import pandas as pd
 
-year = 2005
-reg_season = pd.read_csv("./data/RegularSeasonDetailedResults.csv", index_col = 2)
-teams = pd.read_csv("./data/Teams.csv", index_col = 0)
-team_mappings = pd.DataFrame.to_dict(teams)
-team_mappings = team_mappings['Team_Name']
-reg_season.rename(team_mappings, inplace = True)
-reg_season = reg_season.sort_index()
+def returnYear(year):
+    reg_season = pd.read_csv("./data/RegularSeasonDetailedResults.csv", index_col = 2)
 
+    teams = pd.read_csv("./data/Teams.csv", index_col = 0)
+    team_mappings = pd.DataFrame.to_dict(teams)
+    team_mappings = team_mappings['Team_Name']
+    reg_season.rename(team_mappings, inplace = True)
+    reg_season = reg_season.sort_index()
+    table = None
 
-table = None
+    reg_season = reg_season[reg_season['Season'] == year]
 
-
-reg_season = reg_season[reg_season['Season'] == year]
+    j = ""
+    k = 0
+    for i in reg_season.index:
+        if k == 0:
+            k = 1
+            table = returnRow(i, reg_season)
+            j = i
+        if k == 1 and i!=j: 
+            table = pd.concat([table, returnRow(i, reg_season)])
+            j = i
+    return table;
 
 def returnRow(team_name, reg_season):
-    team_data = reg_season.loc[team_name,:]
+    team_data = reg_season.loc[[team_name],:]
     avg = (team_data.mean()).to_frame()
     avg = avg.rename(columns = {0:team_name})
     avg = avg.transpose()
     return avg
-
-
-j = ""
-k = 0
-for i in reg_season.index:
-    if k == 0:
-        k = 1
-        print(i)
-        table = returnRow(i, reg_season)
-        j = i
-    if k == 1 and j!=i:
-        print(i)
-        table = pd.concat([table, returnRow(i, reg_season)])
-        j = i
-
-
 
