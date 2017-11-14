32606be93447c9563a91165a88003482f2eec2b5
rammalyala <32468151+rammalyala@users.noreply.github.com>
2017-11-13 20:25:20 -0800

    Add files via upload

---
+++ First Model/ML.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+from winlossratio import wlr
+from stats import returnYear
+import sklearn
+from sklearn.linear_model import LinearRegression
+
+table = returnYear(2003)
+table['Y'] = wlr(2003)
+
+for i in range(2003, 2004):
+
+    year = i
+    
+    if i == 2003:
+        table = returnYear(year)
+        table['Y'] = wlr(year)
+
+    else:
+        #table = pd.concat([table, ])
+        table1 = returnYear(year)
+        table1['Y'] = wlr(year)
+        table = pd.concat([table, table1])
+
+
+    table.fillna(value = -1, inplace = True)
+    table = table[table['Y'] != -1]
+    
+Y = table.Y
+
+X = table
+X.drop('Y', axis = 1, inplace = True)
+X.drop('Lteam', axis = 1, inplace = True)
+X.drop('Lscore', axis = 1, inplace = True)
+X.drop('Daynum', axis = 1, inplace = True)
+X.drop('Season', axis = 1, inplace = True)
+X.drop('Lftm', axis = 1, inplace = True)
+X.drop('Lfta', axis = 1, inplace = True)
+X.drop('Lor', axis = 1, inplace = True)
+X.drop('Ldr', axis = 1, inplace = True)
+X.drop('Last', axis = 1, inplace = True)
+X.drop('Lto', axis =1, inplace = True)
+X.drop('Lstl', axis = 1, inplace = True)
+X.drop('Lblk', axis = 1, inplace = True)
+X.drop('Lpf', axis = 1, inplace = True)
+#Initially did not drop:
+X.drop('Lfgm', axis = 1, inplace = True)
+X.drop('Lfga', axis = 1, inplace = True)
+X.drop('Lfgm3', axis = 1, inplace = True)
+X.drop('Lfga3', axis = 1, inplace = True)
+
+
+
+lm = LinearRegression()
+lm.fit(X, Y)
+prediction = lm.predict(X)
+Output = Y.to_frame()
+Output['Prediction'] = prediction
+Output['Percent Error'] = (Output['Prediction'] - Output['Y']) * 100/Output['Y']
+Output.sort_values('Prediction', ascending = False, inplace = True)
+csv = Output.to_csv('output.csv')
+
---
+++ First Model/winlossratio.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def wlr(year):
+    tourney = pd.read_csv("./data/TourneyCompactResults.csv", index_col = 0)
+
+    teams = pd.read_csv("./data/Teams.csv", index_col = 0)
+
+    tourney_2015 = tourney.loc[[year],:]
+
+    wins_2015 = tourney_2015['Wteam'].value_counts(dropna = True).to_frame()
+
+    #losses_2015 = tourney_2015['Lteam'].value_counts(dropna = True).to_frame()
+
+    team_mappings = pd.DataFrame.to_dict(teams)
+    team_mappings = team_mappings['Team_Name']
+
+    wins_2015.rename(team_mappings, inplace=True)
+    #losses_2015.rename(team_mappings, inplace=True)
+
+    wins_2015.columns = ['Wins']
+
+    #losses_2015.columns = ['Losses']
+    #print(losses_2015)
+
+    wins_2015.fillna(value = 0, inplace = True)
+    return wins_2015;
